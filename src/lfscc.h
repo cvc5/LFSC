fa105c3bbf6c8a7567d3a9e4a86545b78b69515f
Andres Noetzli <andres.noetzli@gmail.com>
2019-02-20 02:58:26 -0800

    Maintain backwards compatibility

--- src/lfscc.h
+++ src/lfscc.h
@@ -9,6 +9,17 @@ class libwriter;
 
 void lfscc_init();
 
+void lfscc_check_file(const char* filename,
+                      bool show_runs,
+                      bool no_tail_calls,
+                      bool compile_scc,
+                      bool compile_scc_debug,
+                      bool run_scc,
+                      bool use_nested_app,
+                      bool compile_lib,
+                      sccwriter* scw = NULL,
+                      libwriter* lw = NULL);
+
 void lfscc_check_file(std::istream& in,
                       bool show_runs,
                       bool no_tail_calls,
